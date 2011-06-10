3ed98d3d394baf010d31b6f58509d61ead0cdf07
boris <boris@boris-laptop>
2011-06-10 18:16:04 +0400

    configuration support added

--- src/play/modules/liftmongorecord/LiftMongoRecordPlugin.scala
+++ src/play/modules/liftmongorecord/LiftMongoRecordPlugin.scala
@@ -2,14 +2,28 @@ package play.modules.liftmongorecord
 import play.PlayPlugin
 import play.Play
 import play.Logger
-import com.mongodb.Mongo
+import com.mongodb.{Mongo, MongoOptions, ServerAddress}
 import net.liftweb.mongodb.{DefaultMongoIdentifier, MongoDB}
 
 
 
 class LiftMongoRecordPlugin extends PlayPlugin {
     override def onConfigurationRead() {
-    	MongoDB.defineDb(DefaultMongoIdentifier, new Mongo, "test")
+    	
+    	val host = Play.configuration.getProperty("mongo.host", "localhost");
+    	val port =  Integer.parseInt(Play.configuration.getProperty("mongo.port", "27017"));
+    	val dbname = Play.configuration.getProperty("mongo.database", "play." + Play.configuration.getProperty("application.name"))
+    	val srvr = new ServerAddress(host, port)
+    	val username = Play.configuration.getProperty("mongo.username", null);
+    	val password= Play.configuration.getProperty("mongo.password", null);
+    	val mo = new MongoOptions
+    	mo.socketTimeout = Integer.parseInt(Play.configuration.getProperty("mongo.timeout", "10"));
+    	if(username==null) {
+    		MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(srvr, mo), dbname)
+    	} else {
+    		MongoDB.defineDbAuth(DefaultMongoIdentifier, new Mongo(srvr, mo), dbname, username, password)
+    	}
+    	
     	//Logger.info("scala probably ok")
     	//Logger.info(MongoDB.getDb(DefaultMongoIdentifier).toString);
     	}
